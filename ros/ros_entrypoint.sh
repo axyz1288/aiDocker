086791fc15d424e41feb99c634fcf848a97c43d5
Eric_Chang <axyz1350@gmail.com>
2021-01-25 15:57:37 +0800

    Update: cpp add realsense ros

--- ros/ros_entrypoint.sh
+++ ros/ros_entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 if [ ! -f "~/.bashrc" ] 
 then
-	cp /etc/skel/.bashrc ~/
-	echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+    cp /etc/skel/.bashrc ~/
+    echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 fi
 # setup ros environment
 source /opt/ros/$ROS_DISTRO/setup.bash
